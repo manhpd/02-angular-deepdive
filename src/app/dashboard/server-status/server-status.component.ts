40f7308085aa5067eb80a821e3a7bc3ce74631f2
manh <manhpd.hut@gmail>
2024-09-26 16:56:05 +0700

    Minor changes

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus = 'offline';
+  // private interval? : ReturnType<typeof setInterval>;
+  private destroyRef = inject(DestroyRef);
+
+  constructor() { 
+    
+  }
+
+  ngOnInit(): void {
+    const interval  = setInterval(() => {
+      this.currentStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
+  }
+
+  // ngOnDestroy(): void {
+  //   clearTimeout(this.interval);
+  // }
 }
